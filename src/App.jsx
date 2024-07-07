8c15bca885cf2c4c1203fd2c3fb0f10f3fc96ab2
denikanth <denikanth5244@gmail.com>
2024-07-07 20:30:10 +0530

    install react js and tailwind css and create the first templete

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const App = () => {
+
+  console.log("hello")
+  console.log();
+  return (
+    <div className='w-[100vw] h-[100vh]'>
+      <div className='text-red-700 text-[40px] text-center'>
+        giihn
+      </div>
+    </div>
+  )
+}
+
+export default App
