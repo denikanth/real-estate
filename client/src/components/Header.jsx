d110e1f0921cfd4e9849cebf566a9199f8a71fa0
denikanth <denikanth5244@gmail.com>
2024-07-08 23:15:53 +0530

    create and run the server

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { FaSearch } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
+const Header = () => {
+	return (
+		<header className='bg-[#8697c4] shadow-md '>
+			<div className='flex justify-between items-center max-w-6xl p-3 mx-auto '>
+				<Link to='/'>
+					<h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
+						<span className='text-slate-500'>Deni</span>
+						<span className='text-slate-700'>Estate</span>
+					</h1>
+				</Link>
+
+				<form className='bg-[#dbdee7e9] p-2 rounded-lg flex justify-between items-center gap-2' >
+
+					<input type="text" placeholder='Search..' className='bg-transparent focus:outline-none
+                w-24 sm:w-64 ' />
+					<FaSearch className='text-slate-600 cursor-pointer' />
+				</form>
+				<ul className='flex gap-4'>
+					<Link to='/'>
+						<li className='cursor-pointer   font-[480] hidden sm:inline '>Home</li>
+					</Link>
+					<Link to='/about'>
+						<li className='cursor-pointer  font-[480] hidden sm:inline'>About</li>
+					</Link>
+					<Link to='/sign-in'>
+						<li className='cursor-pointer font-[480] '>SignIn</li>
+					</Link>
+				</ul>
+			</div>
+		</header>
+	)
+}
+
+export default Header
