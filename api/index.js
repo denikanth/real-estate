1cc8c291df4aa85ccf0c535e359a068ca6940bc3
denikanth <denikanth5244@gmail.com>
2024-07-09 20:51:47 +0530

    created a model

--- api/index.js
+++ api/index.js
@@ -1,10 +1,10 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from 'dotenv'
-const app=express()
+const app = express()
 dotenv.config()
 
-mongoose.connect(process.env.MONGO).then(()=>console.log("database is connected")).catch
-((err)=>console.log(err))
+mongoose.connect(process.env.MONGO).then(() => console.log("database is connected")).catch
+    ((err) => console.log(err))
 
-app.listen(3000,()=>{console.log('server is running on port 3000')})
+app.listen(3000, () => { console.log('server is running on port 3000') })
---
+++ api/model/user.model.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+}, { timestamps: true })
+
+const User = new mongoose.model('User', userSchema)//i created a model or collection called User using that userSchema 
+//note that mongoDb automatically add s after User
+export default User
